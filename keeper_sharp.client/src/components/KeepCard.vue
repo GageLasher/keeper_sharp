a110bb9dfa8cfa31e790dcf1e430948656aebd91
Gage Lasher <rsgage21@gmail.com>
2022-04-17 19:59:23 -0600

    commit

--- keeper_sharp.client/src/components/KeepCard.vue
+++ keeper_sharp.client/src/components/KeepCard.vue
@@ -1,14 +1,14 @@
 <template>
   <div
-    class="project m-2"
+    class="project m-3 container rounded shadow p-0"
     @click="setActive"
     data-bs-toggle="modal"
     data-bs-target="#active-keep"
   >
-    <img :src="keep.img" alt="" class="img-fluid" />
-    <div class="row">
+    <img :src="keep.img" alt="" class="img-fluid rounded" />
+    <div class="row bottom-right">
       <div class="col-12 d-flex justify-content-between align-items-center">
-        <h6 class="p-1">{{ keep.name }}</h6>
+        <h5 class="p-1">{{ keep.name }}</h5>
         <img :src="keep.creator.picture" alt="" class="img-fluid pp p-1" />
       </div>
     </div>
@@ -59,5 +59,22 @@ export default {
   border-radius: 50%;
   width: 5vh;
   height: 5vh;
+}
+.container {
+  position: relative;
+
+  color: white;
+
+  background: linear-gradient(to top, rgba(0, 0, 0, 0.85), transparent);
+  text-shadow: 2px 2px #2c2a2a;
+}
+
+.bottom-right {
+  position: absolute;
+  bottom: 8px;
+  right: 16px;
+}
+h5 {
+  font-weight: bold;
 }
 </style>
